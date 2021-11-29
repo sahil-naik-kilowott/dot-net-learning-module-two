f7fea25e056e56561343f445ec14e29435242acc
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-11-29 15:27:38 +0530

    Add project files.

---
+++ module-two/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//STORING VARAIABLES
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+
+
+//ACCESSING THE VARIABLES
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
